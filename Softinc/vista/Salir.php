fbd4c67053937cbdfcaef7df338c2be73a50c7fe
nairare <nairita_15@hotmail.com>
2013-11-19 17:26:38 -0400

    2

--- Softinc/vista/Salir.php
+++
@@ -1,9 +0,0 @@
-<?php
-
- //session_start();
-  unset($_SESSION["nombre_usuario"]);
-  //session_destroy();
-  echo '<script>window.location="../vista/pag-principal/index.php"</script>';
-  //header("Location: ./pag-principal/index.php");
-  //exit;
-?>
