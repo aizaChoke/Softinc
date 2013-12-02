11d741f93b6b6e1b741b2ca83549dd7771882cea
aizaChoke <prodigy_0-07@hotmail.com>
2013-12-02 00:23:26 -0400

    version alfa 2

--- Softinc/vista/CrearCompetencia.php
+++ Softinc/vista/CrearCompetencia.php
@@ -51,24 +51,7 @@
 </script>
     </head>
     <body>
-<<<<<<< HEAD
-        
-        <form action="../controlador/Competencia.php" method="post" onsubmit="return fechas();">
-        <h1>Cree una nueva competencia</h1>
-        <div id="formulario">
-        Nombre de la competencia:<input type="text"             name="nombre_competencia"><br>
-        Fecha de inicio:<input type="date" id='fecha_inicio'    name="fecha_inicio"><br>
-        Hora de inicio:<input type="text" id='hora_ini'             name="hora_ini" onKeyPress="return FormatoHora(event,this)"><br> 
-        Fecha de fin:<input type="date" id='fecha_fin'          name="fecha_fin"><br>
-        Hora de fin:<input type="text" id='hora_fin'            name="hora_fin" onKeyPress="return FormatoHora(event,this)"><br>
-        
-        
-        Configuracion de lenguaje:<br> 
-                        java:<input type="CHECKBOX" name="configurador[]" value='1'>
-                        c:   <input type="CHECKBOX" name="configurador[]" value='2'>
-                        c++: <input type="CHECKBOX" name="configurador[]" value='3'><br>
-        <input type="submit" value="Crear Competencia" name="crear_competencia">
-=======
+       
         <form action="../controlador/Competencia.php" method="post" onSubmit="return fechas();">
           
         <h1 align="center">Cree una nueva competencia</h1>
@@ -88,7 +71,6 @@
            <td>
           </table>
           <input type="submit" value="Crear Competencia" name="crear_competencia">
->>>>>>> a838b74044056ab3e0b3353636c11e2b31f34031
         </div>
         </form>
         
